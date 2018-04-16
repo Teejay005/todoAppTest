56d54545d43af7107e17672316b6966671bc49ef
Adetunji Sunmonu <tunji.sunmonu@gmail.com>
2018-04-16 13:37:24 +1000

    changed app service name

--- app.tf
+++ app.tf
@@ -1,6 +1,5 @@
 
 provider "azurerm" {
-	
 }
 
 resource "azurerm_resource_group" "test" {
@@ -20,7 +19,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "todo-app-service"
+  name                = "todoApptestx"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
